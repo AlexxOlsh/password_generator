1eac26413dc4cf3cd3fd30913478ae49d0df7106
sagirov-coder006 <coder006@noproblemsite.ru>
2024-05-18 20:25:02 +0300

    Create base code

---
+++ main.py
@@ -0,0 +1,94 @@
+from password_class import Password, hide_password
+
+try:
+    pass_len = int(input('Укажите длину пароля (не менее 6 символов и не более 40 символов) '))
+    if pass_len < 6:
+        print('Ошибка: Длина пароля должна быть не меньше 6')
+        exit()
+    elif pass_len > 40:
+        print('Ошибка: Слишком длинный пароль')
+        exit()
+except ValueError:
+    print('Ошибка: необходимо ввести число')
+    exit()
+
+try:
+    passwords_count = int(input('Укажите количество вариантов паролей (не более 10) '))
+    if passwords_count < 1:
+        print('Ошибка: Слишком маленькое количество паролей')
+        exit()
+    elif passwords_count > 10:
+        print('Ошибка: Недопустимое количество вариантов')
+        exit()
+except ValueError:
+    print('Ошибка: необходимо ввести число')
+    exit()
+
+try:
+    user_choose = int(input('Настроить пароль? 1 - Да, 0 - Нет: '))
+    if user_choose != 0 and user_choose != 1:
+        print('Ошибка: Для ввода допустимы 1 или 0')
+        exit()
+except ValueError:
+    print('Ошибка: необходимо ввести число')
+    exit()
+
+password_params = None
+
+if user_choose == 1:
+    try:
+        upper = int(input('Добавить заглавные буквы? 1 - Да, 0 - Нет: '))
+        if upper != 0 and upper != 1:
+            print('Ошибка: Для ввода допустимы 1 или 0')
+            exit()
+    except ValueError:
+        print('Ошибка: необходимо ввести число')
+        exit()
+
+    try:
+        numbers = int(input('Добавить цифры? 1 - Да, 0 - Нет: '))
+        if numbers != 0 and numbers != 1:
+            print('Ошибка: Для ввода допустимы 1 или 0')
+            exit()
+    except ValueError:
+        print('Ошибка: необходимо ввести число')
+        exit()
+
+    try:
+        specials = int(input('Добавить спецсимволы? 1 - Да, 0 - Нет: '))
+        if specials != 0 and specials != 1:
+            print('Ошибка: Для ввода допустимы 1 или 0')
+            exit()
+    except ValueError:
+        print('Ошибка: необходимо ввести число')
+        exit()
+
+    password_params = [upper, numbers, specials]
+
+
+password = Password(pass_len, passwords_count, password_params)
+
+password_result = password.generate_password()
+
+if len(password_result) == 1:
+    try:
+        user_choose = int(input('Вывести пароль на экран или сохранить в файл? 1 - на экран, 0 - в файл: '))
+        if user_choose != 0 or user_choose != 1:
+            print('Ошибка: Для ввода допустимы 1 или 0')
+            exit()
+    except ValueError:
+        print('Ошибка: необходимо ввести число')
+        exit()
+
+    if user_choose == 1:
+        print(f'Ваш пароль: {password_result[0]}     {password.check_password(password_result[0])}')
+    else:
+        secret_word = hide_password(password_result[0])
+        all_passwords = ''
+        with open('password.txt', 'a', encoding='utf-8') as open_file:
+            open_file.write(password_result[0]+'\n')
+        print(f'Пароль {secret_word} успешно сохранен')
+else:
+    print(f'Ваши пароли:')
+    for p in password_result:
+        print(f'{p}     {password.check_password(p)}')
---
+++ password_class.py
@@ -0,0 +1,112 @@
+import string
+import secrets
+import random
+
+
+def hide_password(password):
+    secret_word = ''
+    for c in password:
+        secret_word += '*'
+    return secret_word
+
+
+
+class Password:
+    # params Uppercase, numbers, specials
+    def __init__(self, pass_len, passwords_count, params=None):
+        if params is None:
+            params = [0, 0, 0]
+        self.pass_len = pass_len
+        self.params = params
+        self.symbols = string.ascii_lowercase
+        self.passwords_count = passwords_count
+
+        if self.params[0] == 1:
+            self.symbols += f'{string.ascii_uppercase}'
+
+        if self.params[1] == 1:
+            self.symbols += f'{(string.digits)}'
+
+        if self.params[2] == 1:
+            self.symbols += f'{string.punctuation}'
+
+        self.symbols = ''.join(random.sample(self.symbols, len(self.symbols)))
+
+    def generate_password(self):
+        passwords = []
+        password = ''
+        symbols_count = 0
+        upper_count = 0
+        lower_count = 0
+        numbers_count = 0
+        for i in range(self.passwords_count):
+            while len(password) < self.pass_len:
+                ch = secrets.choice(self.symbols)
+                if ch.islower():
+                    lower_count += 1
+                if self.params[0] and ch.isupper():
+                    upper_count += 1
+                elif self.params[1] == 1 and ch.isdigit():
+                    numbers_count += 1
+                elif self.params[2] == 1 and string.punctuation.find(ch) != -1:
+                    symbols_count += 1
+
+                if len(password) > self.pass_len/2:
+                    if self.params[0] == 1 and upper_count == 0:
+                        password += f'{(secrets.choice(string.ascii_uppercase))}'
+                        upper_count += 1
+                    elif self.params[1] == 1 and numbers_count == 0:
+                        password += f'{(secrets.choice(string.digits))}'
+                        numbers_count += 1
+                    elif self.params[2] == 1 and symbols_count == 0:
+                        password += f'{(secrets.choice(string.punctuation))}'
+                        symbols_count += 1
+                    else:
+                        password += f'{(secrets.choice(self.symbols))}'
+                else:
+                    password += f'{(secrets.choice(self.symbols))}'
+
+            # passwords.append([password, lower_count, upper_count, numbers_count, symbols_count])
+            passwords.append(password)
+            lower_count = 0
+            upper_count = 0
+            numbers_count = 0
+            symbols_count = 0
+            password = ''
+
+        # return [passwords, lower_count, upper_count, numbers_count, symbols_count]
+        return passwords
+
+    def check_password(self, password):
+        res = 0
+        symbols_count = 0
+        upper_count = 0
+        numbers_count = 0
+        if self.pass_len > 8:
+            res = 1
+
+        for ch in password:
+            if self.params[0] and ch.isupper():
+                upper_count += 1
+            elif self.params[1] == 1 and ch.isdigit():
+                numbers_count += 1
+            elif self.params[2] == 1 and string.punctuation.find(ch) != -1:
+                symbols_count += 1
+
+        if upper_count > 0:
+            res += 1
+
+        if numbers_count > 0:
+            res += 1
+
+        if symbols_count > 0:
+            res += 1
+
+        if res > 3:
+            return 'Сложный'
+
+        if res > 2:
+            return 'Средняя сложность'
+
+        if res <= 2:
+            return 'Легкий'
